f76cea958acfbfbe8157f20e7036ffefdedd191e
dikoleshubh <79848187+dikoleshubh@users.noreply.github.com>
2021-06-05 20:58:46 +0530

    Multi Threading

--- EmpMultithreadTest/Properties/AssemblyInfo.cs
+++ EmpMultithreadTest/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("EmpMultithreadTest")]
+[assembly: AssemblyTitle("EmpMultithreadTest ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
